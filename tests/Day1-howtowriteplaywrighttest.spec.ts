ef3a5a0e5a3169d6e65ac4fc27569988061129ec
Renuka <renuka.khirwadkar_@outlook.com>
2025-01-12 19:31:34 +0530

    first commit

---
+++ tests/Day1-howtowriteplaywrighttest.spec.ts
@@ -0,0 +1,19 @@
+const{test,expect}=require('@playwright/test')
+
+test("My first test",async function({page})
+{
+expect('12').toBe('12')
+
+})
+
+test.skip('My second test',async function({page}){
+
+    expect('100').toBe('101')
+
+})
+
+
+test.only('My third test',async function({page}){
+
+    expect('2.0').toBe('2.0')
+})
